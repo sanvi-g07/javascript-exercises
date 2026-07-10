0a418b538cfbca217558ce09f6bcea1183702dc0
sanvi-g07 <146673251+sanvi-g07@users.noreply.github.com>
2026-07-10 18:45:30 -0400

    solved 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+function repeatString(word, numRepeats) {
+    let newWord = "";
+    for(let i = 0; i < numRepeats; i++) {
+        newWord += word;
+    }
+    if(numRepeats >= 0) {
+        return newWord;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
