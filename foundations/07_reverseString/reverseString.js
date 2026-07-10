7302343e013463ac248954919dcc17e6240ed075
sanvi-g07 <146673251+sanvi-g07@users.noreply.github.com>
2026-07-10 18:49:14 -0400

    solved 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+function reverseString(word) {
+    let revWord = "";
+    for(const char of word) {
+        revWord = char + revWord;
+    }
+    return revWord;
 };
 
 // Do not edit below this line
