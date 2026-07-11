94037e53570b0b43d5d23d3cc8ec43e64ea7834a
sanvi-g07 <146673251+sanvi-g07@users.noreply.github.com>
2026-07-11 03:11:47 -0400

    solve 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        let oldAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let newAge = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        if(arr[i].yearOfDeath === undefined) {
+            newAge = 2026 - arr[i].yearOfBirth;
+        }
 
+        if(oldest.yearOfDeath === undefined) {
+            oldAge = 2026 - oldest.yearOfBirth;
+        }
+
+        if(newAge > oldAge) {
+            oldest = arr[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
